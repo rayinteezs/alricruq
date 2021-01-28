31fc12c6e6d4b3ce41c085166e7b2c7d7a3eac74
rayinteezs <a1104512583@sina.com>
2021-01-29 05:11:59 +0800

    version 8.3

--- frontend/src/app/pages/menu/menu.page.ts
+++ frontend/src/app/pages/menu/menu.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterEvent } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
+import { User } from 'src/app/auth/user';
 
 @Component({
   selector: 'app-menu',
